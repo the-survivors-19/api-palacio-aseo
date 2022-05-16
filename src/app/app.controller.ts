020c75bacd1bfd87911ebdf53d794b79c1d23c54
Yesid Hernandez <yexid.9901@gmail.com>
2022-05-16 07:37:40 -0500

    encrypt password in register

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,6 +1,7 @@
 import { BadRequestException, Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
 import { AuthService } from 'src/auth/auth.service';
 import { LocalAuthGuard, JwtAuthGuard } from 'src/auth/guards';
+import { bcrypt } from 'src/helpers';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
 import { UsersService } from 'src/users/users.service';
 import { LoginDto } from './dto/login.dto';
@@ -22,7 +23,9 @@ export class AppController {
   async register(@Body() createUserDto: CreateUserDto){
     const existUser = await this.userService.findEmail(createUserDto.email);
     if (existUser) throw new BadRequestException({ response: 'This email already exist, if you forgot the password please contact with the admin.' });
+    const password = createUserDto.password;
+    createUserDto.password = await bcrypt.encryptPassword(createUserDto.password);
     await this.userService.create(createUserDto);
-    return this.login({ email: createUserDto.email, password: createUserDto.password });
+    return this.login({ email: createUserDto.email, password });
   }
 }
